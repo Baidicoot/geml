08ad8882b45a3f8165c45ba3781cc2cb96ffaada
Aidan K. Ewart <aidanprattewart@gmail.com>
2019-03-03 20:39:35 +0000

    you can now use '$' in text!

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,12 @@ use std::io;
 use std::fs;
 use std::fmt;
 
-//mod markdown;
+lazy_static!{
+    static ref HEAD: Regex = reg(r"^\$(.+?)\$$");
+
+    static ref TAGS: Regex = reg(r"^#\[(.+?)\((.+?)\)\]");
+    static ref RMWS: Regex = reg(r"\s*([\s\S]*)\s*");
+}
 
 #[derive(Debug)]
 #[derive(Clone)]
@@ -111,11 +116,7 @@ fn reg(s: &str) -> regex::Regex {
 
 impl Geml {
     pub fn deserialize(s: String) -> Result<Vec<Geml>> {
-        lazy_static!{
-            static ref TAGS: Regex = reg(r"^#\[(.+?)\((.+?)\)\]");
-            static ref RMWS: Regex = reg(r"\s*([\s\S]*)\s*");
-        }
-        Ok(s.split('$').collect::<Vec<&str>>()[1..]
+        /* Ok(s.split("$").collect::<Vec<&str>>()[1..]
             .chunks(2)
             .filter(|x| (x.len() == 2))
             .map(|x| {
@@ -134,7 +135,36 @@ impl Geml {
                     value,
                     tags,
                 }
-            }).collect())
+            }).collect()) */
+        
+        let mut indexes = HEAD.find_iter(&s)
+            .map(|x| {
+                vec![x.start(), x.end()]
+            })
+            .collect::<Vec<Vec<usize>>>();
+        indexes.push(vec![s.len()]);
+        let tmp: Vec<&usize> = indexes.iter()
+            .flatten()
+            .collect();
+        Ok(tmp.windows(3)
+            .step_by(2)
+            .filter(|x| { x.len() == 3 })
+            .map(|x| {
+                let string = &s[*x[1]..*x[2]];
+                let mut text_start = 0;
+                let mut tags = HashMap::new();
+                for cap in TAGS.captures_iter(&string) {
+                    tags.insert(cap[1].to_string(), cap[2].to_string());
+                    text_start = cap.get(0).unwrap().end();
+                }
+                let value = string[text_start..].trim().to_string();
+                Geml {
+                    key: s[*x[0]..*x[1]].to_string(),
+                    value,
+                    tags,
+                }
+            })
+            .collect())
     }
 
     pub fn parse(&self) -> Result<Geml> {
